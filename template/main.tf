7d43698bf920e8db396f5e47a1c760f8ceb3e4b2
nely <nely@cloudify.co>
2022-02-22 11:05:48 +0200

    fix with tfsec.yaml

--- template/main.tf
+++ template/main.tf
@@ -36,22 +36,8 @@ resource "aws_security_group" "example_security_group" {
   vpc_id      = aws_vpc.example_vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
