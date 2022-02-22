a81abcd70bd25b2ddbd47a9fb5c0b63570cc9d89
nely <nely@cloudify.co>
2022-02-22 10:52:58 +0200

    ex2

--- template/modules/ex2/main.tf
+++ template/modules/ex2/main.tf
@@ -1,17 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "app_server" {
